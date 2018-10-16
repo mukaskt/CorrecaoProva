f9b4a8de6303ed70b7da30645336ecfa6a303053
mukaskt <42787162+mukaskt@users.noreply.github.com>
2018-10-16 20:46:31 -0300

    Add files via upload

---
+++ CorrecaoProva/CorrecaoProva/IMC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CorrecaoProva
+{
+    class IMC
+    {
+        private double peso;
+        private double altura;
+        
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+        }
+    
+    public double Calcular(double peso, double altura)
+    {
+        return peso / (altura * altura);
+    }
+        public double Classificar(double imc)
+        {
+            return 0.0;
+        }
+  }
+}
---
+++ CorrecaoProva/CorrecaoProva/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrecaoProva
+{
+    class Program
+    { 
+        static int Maior(int a, int b)
+    {
+            if (a > b)
+            {
+                return a;
+            }
+
+            else
+            {
+                return b;
+            }      
+    }
+        static bool Par(int numero)
+        {
+            int resto = numero % 2;
+            if (resto == 0)
+                return true;
+            else
+                return false;
+        }
+    
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
