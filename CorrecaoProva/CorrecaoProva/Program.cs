92188a42e75b426210e06ed881d405f1f88e063c
mukaskt <42787162+mukaskt@users.noreply.github.com>
2018-10-23 20:54:17 -0300

    Update Program.cs

--- CorrecaoProva/CorrecaoProva/Program.cs
+++ CorrecaoProva/CorrecaoProva/Program.cs
@@ -31,6 +31,31 @@ namespace CorrecaoProva
     
         static void Main(string[] args)
         {
+                 //exercicio 4
+
+            double x = 0;
+            x = Math.Abs(7.5);
+            Console.WriteLine("a: " + x);// ira retornar o valor 7,5
+
+            x = Math.Floor(7.5);
+            Console.WriteLine("b: " + x);//ira retornar o valor 7 e desconsiderara tudo apos a virgula
+
+            x = Math.Abs(0.0);
+            Console.WriteLine("c: " + x);//ira retornar valor 0
+
+            x = Math.Ceiling(0.0);
+            Console.WriteLine("d: " + x);// ira retornar valor 0
+
+            x = Math.Abs(-6.4);
+            Console.WriteLine("e: " + x);//ira retornar o valor de 6,4
+
+            x = Math.Ceiling(-6.4);
+            Console.WriteLine("f: " + x);//ira retornar o valor 6 e desconsiderara tudo apos a virgula
+
+            x = Math.Ceiling(-Math.Abs(-8 + Math.Floor(-5.5)));
+            Console.WriteLine("g: " + x);//ira retornar o valor 14
+
+            Console.ReadKey();
 
         }
     }
